469a519217bb566d30950c5f5474e5206b1db0e6
HASUNUMA Kenji <k.hasunuma@coppermine.jp>
2014-01-06 23:09:17 +0900

    implement DocomoProvider

--- src/main/java/org/yumix/mail/session/impl/DocomoProvider.java
+++ src/main/java/org/yumix/mail/session/impl/DocomoProvider.java
@@ -1,23 +1,78 @@
 package org.yumix.mail.session.impl;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 import org.yumix.mail.session.SessionProvider;
+import org.yumix.mail.session.UserAccount;
 import org.yumix.mail.session.qualifier.Docomo;
 
+/**
+ * @author k.hasunuma - k.hasunuma at coppermine.jp
+ *
+ */
 @Docomo
 public class DocomoProvider implements SessionProvider {
 
+	/**
+	 * The sender address used in "mail.from" properties of JavaMail.
+	 */
+	private String address;
+	
+	/**
+	 * The user name used logged in a mail server.
+	 */
+	private String userName;
+	
+	/**
+	 * The password used logged in a mail server.
+	 */
+	private String password;
+	
+	public DocomoProvider() { }
+	
+	@Inject
+	public DocomoProvider(UserAccount userAccount) {
+		address = userAccount.getFromAddress();
+		userName = userAccount.getUserName();
+		password = userAccount.getPassword();
+	}
+	
+	/* (non Javadoc)
+	 * @see org.yumix.mail.session.SessionProvider#getSession()
+	 */
 	@Override
 	public Session getSession() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		if (address == null || userName == null || password == null) {
+			throw new IllegalStateException("This provider is not initialized yet");
+		}
+		return getSession(address, userName, password);
 	}
 
+	/* (non Javadoc)
+	 * @see org.yumix.mail.session.SessionProvider#getSession(java.lang.String, java.lang.String, java.lang.String)
+	 */
 	@Override
-	public Session getSession(String address, String userName, String password) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public Session getSession(final String address, final String userName, final String password) {
+		Properties props = new Properties();
+		try {
+			props.load(new InputStreamReader(getClass().getResourceAsStream("/docomo.properties")));
+			props.setProperty("mail.from", address);
+		} catch (IOException e) {
+			return null;
+		}
+		return Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		});
 	}
 
 }
